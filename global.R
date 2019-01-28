45aeb2500f1986cef8db4fae5c348582ff3277a4
efons <47121860+efons@users.noreply.github.com>
2019-01-28 14:43:56 -0800

    Add files via upload
    - global.R is for data upload, processing and subsetting. It uploads the xlsx and shp files located in the same directory 
    - ui.R defines the user interface of the app 
    - server.R is the back-end code of the app

---
+++ global.R
@@ -0,0 +1,352 @@
+# EOA's website redesign 
+# Data visualization app 
+# Emilie Fons 
+
+
+# Script 1 - Global Environment 
+
+
+# set wd
+# setwd("F:/SCVURPPP (SC)/SC94 (2018-19)/Website Re-design/R code for webapp")
+
+#publish app 
+#rsconnect::setAccountInfo(name='scvurppp',token= 'B910374A8BB4335B26ECEEBB507FBD59',secret = 'GnPUQAibzBmyN2bS0qbHqxHmp/bt1CCtHOZaSfpw')
+
+
+
+
+# Required libraries 
+
+library(readxl)
+library(plyr)
+library(tidyverse)
+library(shiny)
+library(leaflet)
+library(shinydashboard)
+library(shinyWidgets)
+library(magrittr)
+library(rgdal)
+library(lubridate)
+library(stringr)
+library(scales)
+library(latex2exp)
+library(beanplot)
+library(rsconnect)
+
+
+
+
+#  - Import spatial files to be used for mapping
+################################################################################################################################################
+################################################################################################################################################
+# watersheds delineation 
+sheds <- readOGR(".","SBC_Sheds_SCC_Only", GDAL1_integer64_policy = F) %>%
+  spTransform(CRS("+init=epsg:4326"))
+
+# creeks 
+#creeks <- readOGR("M:/Santa Clara/Hydrography/Surface Waters/Working/scvurppp_creeks.shp") %>%
+# spTransform(CRS("+init=epsg:4326"))
+# is there a file with a ws data field as well? 
+
+
+# sites info 
+sites <- read_excel("sites master file.xlsx", na=c("", "NA", "#N/A"),sheet="copy_01.28") %>%
+  arrange(rmc_id)
+
+
+# make sure shp watershed names are consistent with sites file
+ws <- c("Lower Penitencia Creek", "San Francisquito Creek", "Coyote Creek", "Matadero/Barron Creeks", "Adobe Creek", "Sunnyvale West", "Guadalupe River", "Permanente Creek", "Calabazas Creek", "San Tomas Aquino", "Sunnyvale East","Stevens Creek")
+sites$watershed <- ifelse(!is.na(sites$watershed) & sites$watershed == "San Thomas Aquino", "San Tomas Aquino", sites$watershed)
+sites$watershed <- ifelse(!is.na(sites$watershed) & sites$watershed == "Matadero Creek", "Matadero/Barron Creeks", sites$watershed)
+unique(sites$watershed) %in% ws # NB: Alameda not in SC county 
+sheds <- sheds[!(sheds$SYSTEM %in% c("Coyote and Lower Pen", "Arroyo la Laguna", "Baylands", "Dumbarton South")),] # remove useless ws
+ws_sheds <- unique(sheds$SYSTEM)
+sheds$SYSTEM <- mapvalues(sheds$SYSTEM, from=ws_sheds, to=ws) # change ws names in shapefile to make them the same as in data file 
+
+# Make sure creek names are consistent with sites file 
+# later 
+#vars_creek <- unique(creeks$NAME)
+#vars_creek # 196 creeks
+
+
+
+
+
+
+# A - Import and reshape bioassessment data 
+##############################################################################################################################
+##############################################################################################################################
+
+# upload data
+
+# data 
+df_bio <- read_excel("data_master_bio_2012_18.xlsx", sheet="All",na=c("","NR", "Not Recorded", "NA"))
+
+
+# select data fields to be plotted and rename
+old_param_names <- c("Elevation (m)", "BMI Taxa", "Diatom Taxa", "CSCI","D18", "ASCI Hybrid", "ASCI diatoms", "Shannon Diversity (H) of Natural Substrate Types",'Percent Substrate Smaller than Sand (<2 mm)',"Percent Impervious", "Road Density Watershed", "Total N", "Chlorophyll a (mg/m2)", '% Macroalgae Cover', "Mean Filamentous Algae Cover", "IPI Score", "Epifaunal Substrate", "Sediment Deposition", "Channel Alteration", "Total PHAB")
+
+select_param <- c("rmc_id","wb_id","year","sample_date", 
+                  "bmi_taxa", "diatom_taxa", "csci", "d18", "h20", "s2","asci_hyb","asci_diatom",
+                  "do_mg_l","ph","alkalinity_mg_l", "sp_cond_us_cm", "temp_c", # water quality
+                  "tn_mg_l", "tp_mg_l", "uia_ug_l", # Nutrients 
+                  "chloro_a_mg_m2", 'pct_macroalg_cvr', "filam_alg_cvr", "afdm_g_m2",# biomass indicators 
+                  "ipi", "tot_phab", # Physical Habitat
+                  "epifaun_substr", "sed_deposition", # PHAB components
+                  "shannon_nst", "pct_smaller_sand", "pct_boulder_ls", "pct_fast_water",# physical habitat metric scores
+                  "crhdi_swamp") 
+
+full_names <- c("RMC Station ID","Water Board ID", "Water Year", "Sample Date",
+                "Total BMI Taxa", "Total Diatom Taxa", "CSCI Score",'D18 IBI Score',"S2 IBI Score","H20 IBI Score","ASCI Hybrid Score", "ASCI Diatom Score",
+                "Dissolved Oxygen (mg/L)", "pH", "Alkalinity (as CaCO3, mg/L)", "Conductivity (uS/cm)", "Temperature (C)",
+                "Total Nitrogen (mg/L)", "Total Phosphorus (mg/L)", "Unionized Ammonia (ug/L)",
+                "Chlorophyll a (mg/m2)", '% Macroalgae Cover', "Mean Filamentous Algae Cover (%)", "AFDM (g/m2)",
+                "IPI Score", "Total PHAB",
+                "Epifaunal Substrate", "Sediment Deposition",
+                'Shannon Diversity (H) of Natural Substrate Types','% Substrate Smaller than Sand (<2 mm)', 'Percent Boulders - large & small', 'Percent Fast Water of Reach',
+                "Human Disturbance Index")
+
+param_names <- data.frame(fullname=c("Watershed","Creek",full_names,"Percent Impervious - Watershed", "Road Density - Watershed"), dataname=c("ws", "creek", select_param, "pct_imperv_ws", "road_dsty_ws")) # add all other parameters from site file
+
+
+# subset bioassessment data
+df_bio <- df_bio %>% filter(year %in% seq(2012,2018,1)) %>% dplyr::select(select_param) %>%
+  arrange(rmc_id) %>%
+  mutate(chloro_a_mg_m2 = as.numeric(chloro_a_mg_m2),
+         epifaun_substr=as.numeric(epifaun_substr),
+         sed_deposition = as.numeric(sed_deposition)) %>% 
+  mutate(lat = sites$lat[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         long = sites$long[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         elevation_m = sites$elevation_m[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         drain_area_km2 = sites$drain_area_km2[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         ws = sites$watershed[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         creek= sites$creek[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         pct_imperv_ws=sites$pct_imperv_ws[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)],
+         road_dsty_ws=sites$road_dsty_ws[which(!(is.na(sites$rmc_id)) & sites$rmc_id %in% rmc_id)]) %>%
+  mutate(pct_imperv_ws =  pct_imperv_ws * 100)
+
+# Variables that will be used in ui 
+bio_vars_yr <- unique(df_bio$year)
+bio_vars_filter <- data.frame(param=c("csci", "asci_hyb", "asci_diatom", 's2',"d18", 'h20',"tot_phab"),
+                          name= c("CSCI Score", "ASCI Hybrid Score", "ASCI Diatom Score","S2 Benthic Algae Score", "D18 Benthic Algae Score","H20 Benthic Algae Score", "Total PHAB"),
+                          threshold1=c(0.63, -1, -1,29, 49,54, 15),
+                          threshold2=c(0.795, -1, -1, 47, 62, 63, 30),
+                          threshold3=c(0.92,-1, -1,60,72,70,46)) # NB: set threshold to -1 when no threshold 
+bio_vars_ws <- sort(factor(unique(sheds$SYSTEM), levels=sort(unique(as.character(sheds$SYSTEM)))))
+
+
+
+
+
+
+
+# B - Import and reshape POC data 
+############################################################################################################################
+############################################################################################################################
+
+colors_PCB <- c("green", "orange", "red", "purple")
+colors_Hg <- c('',"green", "orange", "red")
+
+df_POC <- read_excel("POC_Sediment_Samples.xlsx", sheet="SC")
+df_POC <- df_POC %>% 
+  dplyr::select(1:6,8:9,11,14) %>% 
+  mutate(pcb_conc_cat = factor(pcb_conc_cat, levels=c("<0.2 mg/kg","0.2 - 0.5 mg/kg","0.5 - 1.0 mg/kg",">1.0 mg/kg")),
+         hg_conc_cat = factor(hg_conc_cat, levels=c("None","<0.3 mg/kg","0.3 - 1.0 mg/kg",">1.0 mg/kg"))) %>%
+  mutate(pcb_col = colors_PCB[as.numeric(pcb_conc_cat)], hg_col = colors_Hg[as.numeric(hg_conc_cat)]) %>%
+  mutate(hg_mg_kg = ifelse(hg_mg_kg > 0, hg_mg_kg,0)) %>% 
+  filter(!is.na(samp_date)) %>% 
+  mutate(year = year(samp_date))
+
+poc_vars_yr <- as.numeric(unique(year(df_POC$samp_date)))
+poc_vars_yr <- sort(poc_vars_yr)
+unique(df_POC$site_id) # /!\ duplicate sites <- fix for point visualization 
+
+
+
+
+
+# C - Import and reshape continuous water quality data 
+###########################################################################################################################################
+###########################################################################################################################################
+sites_char <- read_excel("Cont Temp and WQ stations 2012_2018.xlsx", sheet="ALL")
+
+df_wq <- read_excel("data_master_wq_2012_18.xlsx", sheet= "SC WQ ALL") %>%
+  mutate(year = year(date)) %>%
+  mutate(season = factor(ifelse((month(date) == 5 | month(date) == 6 | month(date) == 7), "S", "F"), levels=c("S", "F"))) %>% 
+  mutate(ws = sites_char[match(site_id,sites_char$`Station Number`),"ws"]$ws) %>% 
+  arrange(year)
+
+
+df_temp <-  read_excel("data_master_wq_2012_18.xlsx", sheet= "SC ConTemp") %>% 
+  mutate(year =year(date)) %>% # make factor or ordered num 
+  arrange(year) %>% 
+  mutate(ws = sites_char[match(site_id,sites_char$`Station Number`),"ws"]$ws)
+
+
+# data frame with unique site Id's for mapping 
+all_sites_wq <- unique(c(unique(df_wq$site_id),unique(df_temp$site_id)))
+
+sites_cWQ <- data.frame(site_id = all_sites_wq,
+                        lat= sites_char[match(all_sites_wq,sites_char$`Station Number`),"lat"],
+                        long= sites_char[match(all_sites_wq,sites_char$`Station Number`),"long"],
+                        ws = sites_char[match(all_sites_wq,sites_char$`Station Number`),"ws"],
+                        wq_TF = all_sites_wq %in% unique(df_wq$site_id),
+                        ctemp_TF = all_sites_wq %in% unique(df_temp$site_id),
+                        both_TF = all_sites_wq %in% unique(df_wq$site_id) & all_sites_wq %in% unique(df_temp$site_id)) %>% 
+  mutate(marker_group = ifelse(wq_TF & ctemp_TF, 3, ifelse(ctemp_TF,2,1))) %>% 
+  mutate(sampl_dates_wq = ifelse(marker_group == 1 | marker_group == 3, as.character(paste0(unique(df_wq[which(df_wq$site_id %in% site_id),"year"]),collapse=", ")),
+                                 NA),
+         sampl_dates_temp = ifelse(marker_group == 2 | marker_group == 3, paste0(unique(df_temp[which(df_temp$site_id %in% site_id),"year"]),collapse=", "),
+                                   NA))
+
+# parameters to be used for inputs 
+wq_vars_yr <- as.numeric(unique(year(df_temp$date)))  
+wq_vars_yr_2 <- as.numeric(unique(year(df_wq$date))) 
+wq_vars_yr <- seq(min(c(wq_vars_yr, wq_vars_yr_2)), max(c(wq_vars_yr, wq_vars_yr_2)),1)
+
+wq_vars_ws <-unique(sites_cWQ$ws)
+wq_vars_sub_ws <- unique(sites_cWQ$creek)
+
+# sub-ws groupings 
+wq_GUA_1 <- factor(c('205GUA202', '205GUA205', '205GUA210', '205GUA213', '205GUA218', '205GUA229'))
+wq_GUA_2 <- factor(c('205GUA190', '205GUA250','205GUA255','205GUA262', '205GUA225', '205GUA340', '205GUA270', '205GUA279'))
+wq_STA <- factor(c('205SAR050', '205SAR060', '205SAR070', '205SAR075', '205SAR085'))
+wq_STE <- factor(c('205STE064','205STE065', '205STE070', '205STE071', '205STE095', '205STE105'))
+wq_COY_1 <- sites_cWQ[sites_cWQ$ws=="Coyote Creek","site_id"]
+wq_COY_2 <- factor(wq_COY_1[c(3,2,8,4,1)])
+wq_COY_1 <- factor(wq_COY_1[c(9,6,5,7,10,14,11,12,15,13)])
+
+wq_sites <- unlist(lapply(X=list(wq_GUA_1, wq_GUA_2, wq_STA, wq_STE, wq_COY_1, wq_COY_2),as.character))
+wq_sites <- factor(wq_sites)
+df_temp <- df_temp %>% 
+  mutate(site_id = factor(site_id, levels=wq_sites)) 
+df_temp$plot_cat <- df_temp$site_id %in% wq_GUA_2 +1
+df_wq <- df_wq %>% 
+  mutate(site_id = factor(site_id, levels=wq_sites))
+df_wq$plot_cat <- df_wq$site_id %in% wq_COY_2 +1
+
+# colors for beanplots 
+wq_colors <- rainbow(12)
+
+# Important data 
+MRP_threshold <- data.frame(param = c("temp_c", "ph","sp_cond_us_cm" ,"do_mg_l"),
+                            label = c("Temperature (\u00B0C)", "pH", "Specific Conductivity ($\\mu$S/cm)", "Dissolved Oxygen (mg/L)"), 
+                            value_inf = c(NA,6.5,NA,5), value_sup = c(24,8.5,2000,7), 
+                            units = c("oC", "","uS/cm","mg/L"))
+temp_thresholds <- data.frame(param=c("conTemp",'avDayTemp', "maxDayTemp", "avWeek", "maxWeek"), 
+                              thresh=c(24,17,20,17,21)) # CHECK THRESHOLD VALUES
+
+colors_temp <- colorRampPalette(c("blue","seagreen", "orange", "red"))(11)
+
+
+
+# temperature calculations 
+df_temp_MWAT <- df_temp %>%  
+dplyr::mutate(day1week= as.Date(cut(date, "week"))) %>%
+  dplyr::group_by(year,ws, plot_cat,site_id, day1week) %>% dplyr::summarize(avWeek= mean(ctemp_c))
+df_temp_7DAVG <- df_temp %>% mutate(date =as.Date(date)) %>%
+  dplyr::group_by( year,ws, plot_cat, site_id,date) %>% dplyr::summarize(avDayTemp= mean(ctemp_c)) %>% 
+  mutate(grp=paste(year, site_id,sep=''))
+  
+  
+
+
+
+# E - Customized functions that will be used in the app 
+#############################################################################################################################################
+#############################################################################################################################################
+
+# Custom legend for leaflet maps function 
+addLegendCustom <- function(map, position, colors, labels, sizes, shapes, borders, opacity = 0.5, title=NULL){
+  
+  make_shapes <- function(colors, sizes, borders, shapes) {
+    n <- length(shapes)
+    out <- vector(mode="numeric", length=n)
+    for (i in 1:n){
+      if (!shapes[i]=="triangle"){
+        shapes[i] <- gsub("circle", "50%", shapes[i])
+        shapes[i] <- gsub("square", "0%", shapes[i])
+        out[i] <- paste0(colors[i], "; width:", sizes[i], "px; height:", sizes[i], "px; border:1px solid ", borders[i], "; border-radius:", shapes[i])
+      } 
+      
+      else {out[i] <- paste0("display: block;",
+                             "width: 0;
+                             height: 0;
+                             border-style: solid;
+                             border-width: 0px ", 0.5*sizes[i],"px ", sizes[i],"px " ,0.5*sizes[i], "px;
+                             border-color: transparent transparent ",colors[i]," transparent") }
+      }
+    return(out) 
+      }
+  make_labels <- function(sizes, labels) {
+    paste0("<div style='display: inline-block;height: ", 
+           sizes, "px;margin-top: 4px;line-height: ", 
+           sizes, "px;'>", labels, "</div>")
+  }
+  
+  legend_colors <- make_shapes(colors, sizes, borders, shapes)
+  legend_labels <- make_labels(sizes, labels)
+  
+  return(addLegend(map, position=position ,colors = legend_colors, labels = legend_labels, opacity = opacity, title=title))
+    }
+
+# Custom scatterplots
+ggplotRegression <- function (fit) {
+  
+  lb1 <- paste(sep="", "R-squared =", signif(summary(fit)$r.squared, 4))
+  lb2 <- paste(sep="", "p-value =",signif(summary(fit)$coef[2,4], 3))
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point(col = "blue") + 
+    stat_smooth(method = "lm", col = "grey40", se=F, linetype=2) +
+    ggtitle(paste(lb1,", ", lb2, sep=""))
+  
+}
+
+# custom shapes of markers for leaflet
+addCustomMarkers <- function(map, data, lng, lat, shapes=c(21), bg=c("blue"), size=20, icon_group=1, ...){
+  
+  n = length(shapes)
+  files = character(n)
+  
+  # create a sequence of png images
+  for (i in seq_len(n)) {
+    f = tempfile(fileext = '.png')
+    png(f, width = 30, height = 30, bg = "transparent")
+    par(mar = c(0, 0, 0, 0))
+    plot.new()
+    points(.5, .5, pch = shapes[i], bg=bg[i], cex = 30/8)
+    dev.off()
+    files[i] = f
+  }
+  iconFiles = files
+  return(addMarkers(map=map, data=data, lng=lng, lat=lat, 
+                    icon=~ icons(iconUrl = iconFiles[icon_group], iconWidth = size),...))
+}
+
+
+# add total count on boxplots 
+give_tot <- function(x){
+  return(c(y = median(x)*1.05, label = length(x))) 
+  # experiment with the multiplier to find the perfect position
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################################################################
+##############################################################################################################################################
+##############################################################################################################################################
+
+
+
---
+++ ui.R
@@ -0,0 +1,223 @@
+
+# EOA's website redesign 
+# Data visualization app 
+# Emilie Fons 
+
+
+# Script 2 - User interface
+
+
+# The APP
+
+# User interface 
+ui_db <- dashboardPage(
+  
+  
+  # Header: Page title
+  dashboardHeader(title="Monitoring Data", titleWidth=200),
+  
+  
+  # Sidebar with different types of monitoring data
+  dashboardSidebar(width=200,
+    sidebarMenu(id="menu_items", selected ="water_quality",
+      menuItem("Overview", tabName="overview", icon=icon("home")),
+      menuItem("Water Quality", tabName = "water_quality", icon=icon("tint"), startExpanded = T,
+               menuSubItem("Biomonitoring Data", tabName = "bio_data"),
+               menuSubItem("Continuous Water Quality", tabName = "con_wq"),
+               menuSubItem("Pathogens", tabName= "pathogens"), 
+               menuSubItem("Chlorine", tabName='chlorine'),
+               menuSubItem("Pesticides", tabName= "pesticide")),
+      menuItem("Pollutants of Concern", tabName = "poc", icon=icon("exclamation")),
+      menuItem("Trash", tabName= "trash", icon=icon("trash"))
+    )
+  ),
+  
+  
+  # Body of the webpage 
+  dashboardBody(
+    
+    
+    tags$head(
+      tags$style(
+        HTML('.content-wrapper {
+             overflow-y:scroll
+             }'))),
+    
+    
+    tabItems(
+      
+      tabItem(tabName="overview",
+              h2("Overview"),
+              h5("Welcome to EOA's data visualization dashboard!"),
+              h5("Presentation of the data: blah blah blah"), 
+              h5("Browse the sidebar menu items on the left to explore the data!")),
+      
+      tabItem(tabName = "bio_data",
+              h2("Biological Condition Assessment"),
+             
+              column(6,
+              # Box for inputs 
+               fluidRow(
+
+                  box(width=12, status = "warning", 
+                      div(style="font-weight:blod; color:orange; text-align:center",
+                          fluidRow(h4("Explore Biological Scores and their relationship to Stressors"))),
+                      column(6,selectInput(inputId="filter_by",
+                              label= "Biological Health Score (color):",
+                              choices= c("CSCI Score" = "csci", 
+                                         "ASCI Hybrid Score" = "asci_hyb",
+                                         "ASCI Diatom Score" = "asci_diatom",
+                                         "Benthic Algae Score (S2)"="s2",
+                                         "Benthic Algae Score (H20)"="h20",
+                                         "Benthic Algae Score (D18)" = "d18",
+                                         "Total PHAB" = "tot_phab"
+                              ),selected = "csci")),
+                  column(6,selectizeInput(tags$style(type='text/css', ".selectize-dropdown-content {max-height: 200px; }"),
+                                 inputId="size_by",
+                                 label= "Stressor Variable (size):",
+                                 choices= NULL),
+                         tags$head(tags$style(".leaflet-top {z-index:999!important;}")),
+                         prettyCheckbox(inputId="show_radius", label="Show as marker size?", 
+                                       value=F, shape="round", animation="pulse", fill=F)))
+                  ),
+              
+              # Box for Map
+              box(width=NULL, status='primary',
+                  fluidRow(column(4,sliderInput(inputId = "wy", label=NULL, ticks=T,
+                                                    min=min(bio_vars_yr), max=max(bio_vars_yr),
+                                                    value=c(min(bio_vars_yr),max(bio_vars_yr)), sep="")),
+                            column(4,selectInput(inputId="spatial_filter", label="Spatial Scale:",
+                                                    choices=c("County-level"="whole_county", "Watershed-level"="sub_ws"),
+                                                    selected="whole_county")),
+                            column(4,uiOutput("scnd_sub_ws"))),
+                  fluidRow(column(12,leafletOutput("map_sites"))),
+                  fluidRow(column(3,actionButton("reset_button", "Reset view")),
+                  column(6,radioButtons(inputId="show_creeks", label="Show Creeks", 
+                               choices=c("Yes"= "yes", "No"= "no"),selected="no", inline=T))))), 
+              
+              
+              column(6,
+              # TabBox for outputs 
+              tabBox(id="output_tabs", width=12,
+                    
+                     tabPanel(title="Plots",id="overview_plots",div(style="overflow-y: scroll; height: 700px",
+                              div(style="font-weight:bold", textOutput("ws_list_2")),
+                              br(),
+                              div(style="font-weight:bold", textOutput("scatterplots")),
+                              
+                              uiOutput("cond_scatter"),
+                          
+                              div(style="font-weight:bold", textOutput("boxplots")),
+                              br(),
+                              uiOutput("cond_boxplot"),
+                              plotOutput("boxplot2"))), 
+                     tabPanel(title="Table", id="overview_table", div(style="overflow-y: scroll; height: 700px",
+                              div(style="font-weight:bold", textOutput("ws_list_1")), 
+                              br(),
+                              div(textOutput("score_tables"), style="font-weight:bold"),
+                              br(),
+                              div(uiOutput("cond_table"), style = "font-size:90%"),
+                              tags$head(tags$style("#cond_table td{
+                                                   position:relative;
+                                                   };"))
+                              )),
+                     
+                     tabPanel(title="Watershed-specific", id="ws_info",
+                              h5("Click on a watershed to get detailed information"),
+                              textOutput("ws_info")),
+                     
+                     tabPanel(title="Site-specific", id="site_info",
+                              h5("Click on a site on the map to visualize site-specific information"),
+                              textOutput("site_info"),
+                              tableOutput("table_site_onClick"))
+                     
+                              ))       
+              ),
+              
+      
+      
+      
+      tabItem(tabName = "con_wq", 
+              h2("Continuous Water Quality & Continuous Temperature"),
+              
+              # Box for inputs 
+             fluidRow(box(width=3, sliderInput(inputId = "wq_yr", label="Year:", 
+                                       min=min(wq_vars_yr), max=max(wq_vars_yr), 
+                                       value = c(min(wq_vars_yr),max(wq_vars_yr)), sep=''),
+                           selectInput(inputId = "wq_ws", label="Watershed:", 
+                                        choices=c(wq_vars_ws),selected=wq_vars_ws[1])
+                           ),
+             #box for map
+              box(width=9,leafletOutput("map_wq"))),
+              
+              # Box for plots
+              fluidRow(tabBox(width=12, height=800, id="wq_plots",
+                  tabPanel(title="Continuous Temperature", id="con_temp_plot",
+                  selectInput(inputId = "temp_param", label="Parameter:", 
+                              choices=c("Continuous Temperature" = "ConTemp", "Daily Average"="avDayTemp", "Daily Max"="maxDayTemp", "Weekly Average" = "avWeek", "Weekly Maximum" = "maxWeek"), 
+                              selected="avDayTemp"),
+                  plotOutput("temp_timeseries_1"),
+                  plotOutput("temp_timeseries_2")),
+                  tabPanel(title="Seasonal Water Quality Parameters", id="wq_plot",
+                           fluidRow(column(3,selectInput(inputId = "wq_param", 
+                                       label = "Water Quality Parameter:", 
+                                       choices=c("Temperature" = "temp_c", "pH"="ph" , "Specific Conductivity" = "sp_cond_us_cm", "Dissolved Oxygen" = "do_mg_l"),
+                                       selected='ph')),
+                           column(3,selectInput(inputId = "wq_season", 
+                                       label = "Season:", 
+                                       choices=c("Spring"="S", "Fall"="F"), selected="S"))),
+                           fluidRow(column(6,plotOutput("wq_boxplot_1"),
+                           plotOutput("wq_boxplot_2")),
+                           
+                           column(6,plotOutput("wq_beanplot_1"),
+                           plotOutput("wq_beanplot_2")))
+                  )))),
+              
+             
+      
+      tabItem(tabName = "pathogens", 
+              h2("Pathogens")),
+      
+      tabItem(tabName = "chlorine", 
+              h2("Chlorine")),
+      
+      tabItem(tabName = "pesticide", 
+              h2("Pesticides")),
+      
+      
+      
+      
+      # POC Menu Item 
+      tabItem(tabName = "poc",
+              h2("Pollutants of Concern"),
+              
+              # Box for inputs 
+              fluidRow(box( selectInput(inputId="poc_contaminant",label="Contaminant:", choices=c("Mercury"="hg", "PCB"="pcb"), selected = "hg"),
+                   sliderInput(inputId="poc_yr", label="Time period", min =min(poc_vars_yr), max=max(poc_vars_yr), value=c(min(poc_vars_yr),max(poc_vars_yr)), sep="")),
+             # Box for plots 
+                   box(plotOutput("plot_poc_1", height="200px"))),
+
+              # Box for map 
+              fluidRow(box(width=12,leafletOutput("map_poc")))
+              
+              
+              
+      ),
+      
+      
+      # Trash Menu Item 
+      tabItem(tabName = "trash",
+              h2("Trash Data"))
+    )
+  )
+)
+
+
+
+
+
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+
+
